400ae0432a68bcb96179f9fb1a995494378bdc63
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-07 11:20:32 +0200

    Initial Commit

---
+++ ClassStructureWrappers/ClassStructureWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassStructureWrappersApp.swift
+//  ClassStructureWrappers
+//
+//  Created by Vahtee Boo on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClassStructureWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
